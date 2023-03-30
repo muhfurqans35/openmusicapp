4fc0ddaf38e04660160c9e32e8aef4efd2a84b95
muhfurqans <muhfurqans35@gmail.com>
2023-03-30 11:00:46 +0800

    fixvalidation

--- src/validator/song/schema.js
+++ src/validator/song/schema.js
@@ -4,7 +4,7 @@ const SongPayloadSchema = Joi.object({
   title: Joi.string().required(),
   year: Joi.number().required(),
   genre: Joi.string().required(),
-  performer: Joi.string(),
+  performer: Joi.string().required(),
   duration: Joi.number(),
   albumId: Joi.string(),
 });
